9555a0964ab65c266c02a6d7fa6c4c7b1ff293fb
lmhyuck <49359991+lmhyuck@users.noreply.github.com>
2024-07-11 16:10:45 +0900

    Create TradeStrategyDTO.java

---
+++ Real-BackEnd/src/main/java/com/example/BeFETest/DTO/coinDTO/TradeStrategyDTO.java
@@ -0,0 +1,31 @@
+package com.example.BeFETest.DTO.coinDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public abstract class TradeStrategyDTO {
+
+    protected double initialInvestment;
+    protected double transactionFee;
+    protected LocalDate startDate;
+    protected LocalDate endDate;
+    protected String targetItem;
+    protected String tickKind;
+    protected int inquiryRange;
+
+    public TradeStrategyDTO(double initialInvestment, double transactionFee,
+                            LocalDate startDate, LocalDate endDate, String targetItem, String tickKind, int inquiryRange){
+        this.initialInvestment = initialInvestment;
+        this.transactionFee = transactionFee;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.targetItem = targetItem;
+        this.tickKind = tickKind;
+        this.inquiryRange = inquiryRange;
+    }
+
+}

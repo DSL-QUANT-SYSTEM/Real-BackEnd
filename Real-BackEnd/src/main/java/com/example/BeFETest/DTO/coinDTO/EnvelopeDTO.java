0bfc0cb996c1cc78a6ff22486afa251db5c541b3
lmhyuck <49359991+lmhyuck@users.noreply.github.com>
2024-07-18 21:35:36 +0900

    엔벨로프 전략 추가
    엔벨로프 전략 추가 및 DTO클래스 추가

---
+++ Real-BackEnd/src/main/java/com/example/BeFETest/DTO/coinDTO/EnvelopeDTO.java
@@ -0,0 +1,23 @@
+package com.example.BeFETest.DTO.coinDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class EnvelopeDTO extends TradeStrategyDTO{
+
+    private double moving_up;
+    private double moving_down;
+    private int movingAveragePeriod;
+
+    public EnvelopeDTO(double initialInvestment, double transactionFee, LocalDate startDate, LocalDate endDate,
+                       String targetItem, String tickKind, int inquiryRange,
+                        double moving_down, double moving_up, int movingAveragePeriod) {
+        super(initialInvestment, transactionFee, startDate, endDate, targetItem, tickKind, inquiryRange);
+        this.moving_down=moving_down;
+        this.moving_up=moving_up;
+        this.movingAveragePeriod=movingAveragePeriod;
+    }
+}
